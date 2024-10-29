d84ff845b51fa5d5c9cea61bbd039842608f0d45
Mark Dunne <Markdunne117@gmail.com>
2024-10-29 16:24:49 +0000

    Add media folder and specify file location in settings

--- dunneeco/settings.py
+++ dunneeco/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# Specify static files location
 STATIC_URL = 'static/'
+
+# specify media files location
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

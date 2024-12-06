aa212246006c455d78100424f5e26ed137e14014
Mark Dunne <Markdunne117@gmail.com>
2024-12-06 15:50:39 +0000

    Remove commented code from home views

--- home/views.py
+++ home/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.contrib import messages
 
-# def index(request):
-#     """ This view returns the index page """
-#     return render(request, 'home/index.html')
 
 def index(request):
     """ This view returns the index page """
     return render(request, 'home/index.html')
+    

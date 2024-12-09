8b54ca5c2f84994bf48a7a8664c1d886b1f38a1e
Mark Dunne <Markdunne117@gmail.com>
2024-12-09 12:02:42 +0000

    Add view for contact form and page

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
 
 def contact(request):
-    """ This view returns the index page """
-    return render(request, 'contact/contact.html')
+    """ This view handles submitting the contact form """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Your message has been sent to the team!')
+            return redirect('home')
+        else:
+            messages.error(request, 'There was an error with your submission. Please check the form and try again.')
+    else:
+        form = ContactForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'contact/contact.html', context)

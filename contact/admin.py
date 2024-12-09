750f9b04fec5b99c5bd0c3a23bf5d356d37b3bc1
Mark Dunne <Markdunne117@gmail.com>
2024-12-09 12:13:08 +0000

    Create contact admin class for admin view

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at')
+    search_fields = ('name', 'email', 'subject')
+    list_filter = ('created_at',)
+    readonly_fields = ('name', 'email', 'phone', 'subject', 'message')
+
+    fieldsets = (
+        ('Contact Details', {
+            'fields': ('name', 'email', 'phone')
+        }),
+        ('Message Information', {
+            'fields': ('subject', 'message', 'created_at')
+        }),
+    )
+
+    # Orders by newest messages first
+    ordering = ('-created_at',)
+
+
+admin.site.register(Contact, ContactAdmin)

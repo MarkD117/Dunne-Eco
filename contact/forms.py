4a676fd8320fae5ba37b5cba94c2e1bb023be3f7
Mark Dunne <Markdunne117@gmail.com>
2024-12-09 11:15:58 +0000

    Add contact form.py code

---
+++ contact/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Enter your full name',
+            'email': 'Enter your email address',
+            'phone': 'Optional: Enter your phone number',
+            'subject': 'Enter the subject of your message',
+            'message': 'Type your message here',
+        }
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'contact-form-input'
+            if field in placeholders:
+                self.fields[field].widget.attrs['placeholder'] = placeholders[field]

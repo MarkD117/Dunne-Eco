350948bbcc35e26feaea137fcfeaa35677cc4b08
Mark Dunne <Markdunne117@gmail.com>
2024-12-18 11:33:18 +0000

    Create initial gallery view

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def gallery(request):
+    """ This view returns the gallery page """
+    return render(request, 'gallery/gallery.html')

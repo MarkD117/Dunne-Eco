267b8337e9b99370afae9779c019470b40e30494
Mark Dunne <Markdunne117@gmail.com>
2024-12-18 18:00:50 +0000

    Add inital form fields

---
+++ gallery/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Category, Image
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'friendly_name']
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['category', 'image', 'title']

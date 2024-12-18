e8507749325641a3ed78ff78f63f860dc0bd2284
Mark Dunne <Markdunne117@gmail.com>
2024-12-18 18:01:23 +0000

    Add support for multiple images to gallery form

--- gallery/forms.py
+++ gallery/forms.py
@@ -6,7 +6,13 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ['name', 'friendly_name']
 
+
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['category', 'image', 'title']
+
+    # Add support for multiple images
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].widget.attrs.update({'multiple': True})

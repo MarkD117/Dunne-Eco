329f117918b8cc3525e1c17c8e6d818a43096927
Mark Dunne <Markdunne117@gmail.com>
2024-12-18 18:13:41 +0000

    Update gallery form with slug additions

--- gallery/forms.py
+++ gallery/forms.py
@@ -4,7 +4,19 @@ from .models import Category, Image
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ['name', 'friendly_name']
+        fields = ['name']  # Only the name field is editable; the slug is auto-generated
+
+    def save(self, commit=True):
+        """
+        Override the save method to ensure the slug is generated only once.
+        """
+        instance = super().save(commit=False)
+        if not instance.slug:  # Generate slug if it doesn't already exist
+            from django.utils.text import slugify
+            instance.slug = slugify(instance.name)
+        if commit:
+            instance.save()
+        return instance
 
 
 class ImageForm(forms.ModelForm):
@@ -12,7 +24,7 @@ class ImageForm(forms.ModelForm):
         model = Image
         fields = ['category', 'image', 'title']
 
-    # Add support for multiple images
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Allow multiple image uploads via the widget
         self.fields['image'].widget.attrs.update({'multiple': True})

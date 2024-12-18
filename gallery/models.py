7d1563486511aab873efce408b4a269582be7f21
Mark Dunne <Markdunne117@gmail.com>
2024-12-18 10:41:28 +0000

    Add initial gallery model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254, unique=True)
+    friendly_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Image(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, blank=False, related_name="images")
+    image = models.ImageField(upload_to="gallery/", null=False, blank=False)
+    title = models.CharField(max_length=254, blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title or f"Image {self.id}"

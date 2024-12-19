9ca1ea92dad74ad668ded1502e56de9189905b95
Mark Dunne <Markdunne117@gmail.com>
2024-12-19 16:20:33 +0000

    Register brochure model in admin

--- downloads/admin.py
+++ downloads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Brochure
 
-# Register your models here.
+
+@admin.register(Brochure)
+class BrochureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uploaded_at')
+    search_fields = ('title',)

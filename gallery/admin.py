8073490c219431633b2bec31a1a73c2f8fb2d026
Mark Dunne <Markdunne117@gmail.com>
2024-12-19 13:13:55 +0000

    Register catgory and image models in gallery admin with custom filters

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Image
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    # Slug read-only due to auto-generation
+    readonly_fields = ('slug',)
+    # Slug included as read-only in the form layout
+    fields = ('name', 'slug')
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'uploaded_at')
+    list_filter = ('category',)
+    search_fields = ('title', 'category__name')

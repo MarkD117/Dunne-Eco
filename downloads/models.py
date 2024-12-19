4e4f451e3e087691dde1e2330c55783b18832e38
Mark Dunne <Markdunne117@gmail.com>
2024-12-19 16:19:51 +0000

    Add model for brochure downloads

--- downloads/models.py
+++ downloads/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Brochure(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)  # Optional description
+    brochure_file = models.FileField(upload_to='brochures/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

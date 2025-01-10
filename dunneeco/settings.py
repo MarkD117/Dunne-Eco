dfb1548dfbdbbdc3a7935d1977d2203229dd2f3b
Mark Dunne <Markdunne117@gmail.com>
2025-01-10 19:20:17 +0000

    Edit USE_AWS conditional logic for specificity

--- dunneeco/settings.py
+++ dunneeco/settings.py
@@ -194,7 +194,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if 'USE_AWS' in os.environ:
+if os.environ.get('USE_AWS') == "TRUE":
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',

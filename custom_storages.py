70c0993aca661386dcd31dbd0e44a4356acb30ed
Mark Dunne <Markdunne117@gmail.com>
2025-01-10 15:57:15 +0000

    Add logging to custom_storages.py to troubleshoot AWS static file upload

--- custom_storages.py
+++ custom_storages.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+import logging
+logger = logging.getLogger(__name__)
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+    logger.info(f"Static file location: {location}")
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    logger.info(f"Media file location: {location}")

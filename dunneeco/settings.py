f6dcff8852a09a4e95766b4b5ea25ed249dd0a6c
Mark Dunne <Markdunne117@gmail.com>
2025-01-10 10:13:13 +0000

    Override static and media URLs in production

--- dunneeco/settings.py
+++ dunneeco/settings.py
@@ -203,6 +203,10 @@ if 'USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 # Email Config
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

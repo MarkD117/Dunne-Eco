a63e648eaf2cbee9bb1b1202396e9e6a009e18d0
Mark Dunne <Markdunne117@gmail.com>
2025-01-10 13:41:32 +0000

    Remove storages print statements

--- custom_storages.py
+++ custom_storages.py
@@ -4,14 +4,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
-    def __init__(self, *args, **kwargs):
-        print("Using StaticStorage backend")
-        super().__init__(*args, **kwargs)
-
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-
-    def __init__(self, *args, **kwargs):
-        print("Using MediaStorage backend")
-        super().__init__(*args, **kwargs)
-

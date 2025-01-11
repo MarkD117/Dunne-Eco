acf065d4c4b7053e40c656d8c5b52f7738b86a54
Mark Dunne <Markdunne117@gmail.com>
2025-01-11 17:01:04 +0000

    Add fallbacks for static and media file storage locations

--- custom_storages.py
+++ custom_storages.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    # Set location using `STATICFILES_LOCATION` from settings, but default to 'static' if not defined
+    location = getattr(settings, 'STATICFILES_LOCATION', 'static')
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    # Set location using `MEDIAFILES_LOCATION` from settings, but default to 'media' if not defined
+    location = getattr(settings, 'MEDIAFILES_LOCATION', 'media')

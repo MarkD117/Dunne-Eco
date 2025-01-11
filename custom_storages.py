acc93d335861163fa498730525cb8419a97e7a9a
Mark Dunne <Markdunne117@gmail.com>
2025-01-11 17:11:04 +0000

    Revert custom storages changes

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
 class StaticStorage(S3Boto3Storage):
-    # Set location using `STATICFILES_LOCATION` from settings, but default to 'static' if not defined
-    location = getattr(settings, 'STATICFILES_LOCATION', 'static')
+    location = settings.STATICFILES_LOCATION
+
 
 class MediaStorage(S3Boto3Storage):
-    # Set location using `MEDIAFILES_LOCATION` from settings, but default to 'media' if not defined
-    location = getattr(settings, 'MEDIAFILES_LOCATION', 'media')
+    location = settings.MEDIAFILES_LOCATION

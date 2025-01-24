c4e87be83ed1a3b55f6ccd28cc21515828a04c9a
Mark Dunne <Markdunne117@gmail.com>
2025-01-24 09:06:28 +0000

    Define STATIC_ROOT in settings

--- dunneeco/settings.py
+++ dunneeco/settings.py
@@ -178,6 +178,7 @@ USE_TZ = True
 # Specify static files location
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Collect static files for production
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -214,6 +215,8 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Email Config
 if 'DEVELOPMENT' in os.environ:

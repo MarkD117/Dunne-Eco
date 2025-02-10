62fb37abd0aaa32603501821f83fb6ff2cdff952
Mark Dunne <Markdunne117@gmail.com>
2025-02-10 08:59:51 +0000

    Re add end slash to MEDIA_URL and remove from file declarations

--- dunneeco/settings.py
+++ dunneeco/settings.py
@@ -222,7 +222,7 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Email Config
 if 'DEVELOPMENT' in os.environ:

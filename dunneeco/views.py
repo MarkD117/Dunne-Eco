3fac6b837e6610043e36b24a97b051352d3f91e3
Mark Dunne <Markdunne117@gmail.com>
2025-02-10 16:34:50 +0000

    Add request context to handler404 view

--- dunneeco/views.py
+++ dunneeco/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from django.template import RequestContext
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+    return render(request, "errors/404.html", {}, status=404)

5657d0e0c7772ab7b4f87801a8a854d27c0bd81e
Mark Dunne <Markdunne117@gmail.com>
2025-02-10 16:04:16 +0000

    Add handler 404 url to project urls

--- dunneeco/urls.py
+++ dunneeco/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,5 @@ urlpatterns = [
     path('debug/', include('debug.urls')),
     # Using static function to add media url
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'dunneeco.views.handler404'

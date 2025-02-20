a2a6707b6b5caed34a4f1a2b0c786995291374c9
Mark Dunne <Markdunne117@gmail.com>
2025-02-20 16:28:45 +0000

    Update gallery views to sort images by category

--- gallery/views.py
+++ gallery/views.py
@@ -4,9 +4,16 @@ from .models import Category, Image
 
 def gallery(request):
     categories = Category.objects.all()
+    selected_category = request.GET.get('category')
     images = Image.objects.all()
+
+    # Only filter images if a category is selected
+    if selected_category:
+        images = images.filter(category__slug=selected_category)
+
     context = {
         'categories': categories,
+        'selected_category': selected_category,
         'images': images,
     }
     return render(request, 'gallery/gallery.html', context)

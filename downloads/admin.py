4853d85af10ca0ef5ccdf1008edcd88b4e403c5f
Mark Dunne <Markdunne117@gmail.com>
2025-02-24 09:41:37 +0000

    Add brochure thumbnail to downloads admin.py

--- downloads/admin.py
+++ downloads/admin.py
@@ -5,4 +5,5 @@ from .models import Brochure
 @admin.register(Brochure)
 class BrochureAdmin(admin.ModelAdmin):
     list_display = ('title', 'uploaded_at')
+    fields = ('title', 'description', 'brochure_file', 'thumbnail')
     search_fields = ('title',)

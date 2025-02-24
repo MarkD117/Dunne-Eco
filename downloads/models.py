fd41fd2784536ee90d9224d27c013e25ea4e510b
Mark Dunne <Markdunne117@gmail.com>
2025-02-24 09:40:55 +0000

    Add brochure thumbnail field to downloads model

--- downloads/models.py
+++ downloads/models.py
@@ -5,6 +5,7 @@ class Brochure(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)  # Optional description
     brochure_file = models.FileField(upload_to='brochures/')
+    thumbnail = models.ImageField(upload_to='brochure-thumbnails/', blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

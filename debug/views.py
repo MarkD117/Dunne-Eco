674b42b8b7d8a9d87eb3957450a533de32a3e7f5
Mark Dunne <Markdunne117@gmail.com>
2025-03-11 16:17:31 +0000

    Remove debug app

--- debug/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-from django.conf import settings
-
-def test_s3_storage(request):
-    return JsonResponse({
-        'STATICFILES_STORAGE': settings.STATICFILES_STORAGE,
-        'AWS_STORAGE_BUCKET_NAME': settings.AWS_STORAGE_BUCKET_NAME,
-    })

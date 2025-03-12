530daa5b014bf3fa36fffa68be8b7b9357825b68
Mark Dunne <Markdunne117@gmail.com>
2025-03-12 08:54:16 +0000

    Add extra blank line to project views file

--- dunneeco/views.py
+++ dunneeco/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext
 
+
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", {}, status=404)

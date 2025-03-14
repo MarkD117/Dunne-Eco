80fea49949d4f02ae46c6c210bc0015f8271384e
Mark Dunne <Markdunne117@gmail.com>
2025-03-14 14:24:36 +0000

    Add Google reCAPTCHA to contact forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Contact
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV3
 
 
 class ContactForm(forms.ModelForm):
+
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
+
     class Meta:
         model = Contact
         fields = '__all__'

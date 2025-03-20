326823ba028bd00df3d77808719aa8764a28876f
Mark Dunne <Markdunne117@gmail.com>
2025-03-20 13:34:26 +0000

    Add privacy policy view to home app views.py

--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ from django.contrib import messages
 def index(request):
     """ This view returns the index page """
     return render(request, 'home/index.html')
+
+
+def privacy_policy(request):
+    """ This view returns the privacy policy page """
+    return render(request, 'home/privacy.html')

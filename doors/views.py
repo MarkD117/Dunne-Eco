3bf04ce0e7c24a5f5ef525e0fcb38195c9a5bcf1
Mark Dunne <Markdunne117@gmail.com>
2025-03-26 11:40:11 +0000

    Create doors view

--- doors/views.py
+++ doors/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
- 
-# Create your views here.
+from django.contrib import messages
+
+
+def door_builders(request):
+    """ This view returns the doors page """
+    return render(request, 'doors/doors.html')

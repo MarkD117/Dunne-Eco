23ee7ff102386630622cde0b2b33821d4df01d57
Mark Dunne <Markdunne117@gmail.com>
2025-04-03 14:05:32 +0000

    Update door view class name

--- doors/urls.py
+++ doors/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.door_builders, name='doors'),
+    path('', views.doors, name='doors'),
 ]
--- doors/views.py
+++ doors/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib import messages
 
 
-def door_builders(request):
+def doors(request):
     """ This view returns the doors page """
     return render(request, 'doors/doors.html')

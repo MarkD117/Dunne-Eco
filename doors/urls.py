159101d4a39765d659395dca37d2ec6584906bdf
Mark Dunne <Markdunne117@gmail.com>
2025-04-29 10:19:11 +0000

    Add unavailable feature message for door builder buttons

--- doors/urls.py
+++ doors/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.doors, name='doors'),
+    path('doors/unavailable/', views.unavailable_message, name='unavailable_message'),
 ]
--- doors/views.py
+++ doors/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.urls import reverse
 
 
 def doors(request):
     """ This view returns the doors page """
     return render(request, 'doors/doors.html')
+
+
+def unavailable_message(request):
+    messages.info(request, 'Unfortunately this feature is currently unavailable! If you would like to spec your own custom door, please contact us!')
+    return redirect(reverse('doors'))

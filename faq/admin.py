dd25062854ef1c0305075d1c4996a5f33f5c37cf
Mark Dunne <Markdunne117@gmail.com>
2025-05-01 10:44:20 +0000

    Register FAQ models in admin

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import FAQ, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'category', 'is_active', 'created_at')
+    list_filter = ('is_active', 'category')
+    search_fields = ('question', 'answer')

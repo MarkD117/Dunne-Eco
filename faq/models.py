cf19fb8f276bc68e5db138b2aa5a038076aad7bc
Mark Dunne <Markdunne117@gmail.com>
2025-05-01 10:38:41 +0000

    Create faq models

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.question

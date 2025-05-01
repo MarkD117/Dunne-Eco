50764dcf9b1d3e1da2ce2f22fddcefae27203f5b
Mark Dunne <Markdunne117@gmail.com>
2025-05-01 10:39:54 +0000

    Add categories to faq model

--- faq/models.py
+++ faq/models.py
@@ -1,6 +1,16 @@
-from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class FAQ(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='faqs')
     question = models.CharField(max_length=255)
     answer = models.TextField()
     is_active = models.BooleanField(default=True)

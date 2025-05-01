df13f7e8d9c1d86c9a93f567d444624129b5119a
Mark Dunne <Markdunne117@gmail.com>
2025-05-01 15:02:22 +0000

    Add pagination for FAQ page

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.db.models import Q
 from .models import FAQ, Category
@@ -16,10 +17,14 @@ def faq_list(request):
             Q(question__icontains=query) | Q(answer__icontains=query)
         )
 
+    paginator = Paginator(faqs, 5)  # Show 5 FAQs per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     categories = Category.objects.all()
 
     context = {
-        'faqs': faqs,
+        'page_obj': page_obj,
         'categories': categories,
         'selected_category': category_id,
         'query': query,

3f44d5e4eeb5c4e183fbbd85f294459168fe6032
Mark Dunne <Markdunne117@gmail.com>
2025-05-01 10:53:05 +0000

    Add view code for FAQ page

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import FAQ, Category
 
-# Create your views here.
+def faq_list(request):
+    query = request.GET.get('q')
+    category_id = request.GET.get('category')
+
+    faqs = FAQ.objects.filter(is_active=True)
+
+    if category_id:
+        faqs = faqs.filter(category__id=category_id)
+
+    if query:
+        faqs = faqs.filter(
+            Q(question__icontains=query) | Q(answer__icontains=query)
+        )
+
+    categories = Category.objects.all()
+
+    context = {
+        'faqs': faqs,
+        'categories': categories,
+        'selected_category': category_id,
+        'query': query,
+    }
+    return render(request, 'faq/faq_list.html', context)

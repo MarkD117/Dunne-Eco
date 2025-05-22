0de2a1354ea584142da96188525ed8497a4be097
Mark Dunne <Markdunne117@gmail.com>
2025-05-22 15:22:38 +0000

    Add URL pattern for category_detail view with slug-based routing

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'gallery'
+
 urlpatterns = [
-    path('', views.gallery, name='gallery')
+    path('', views.gallery, name='gallery'),
+    path('category/<slug:slug>/', views.category_detail, name='category_detail'),
 ]

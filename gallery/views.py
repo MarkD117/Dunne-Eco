a9e27012d911dcbc612868bf0104f2a5db7178ff
Mark Dunne <Markdunne117@gmail.com>
2025-05-22 15:20:27 +0000

    Refactor gallery view and add category_detail view for SEO-friendly URLs

--- gallery/views.py
+++ gallery/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 from .models import Category, Image
+from django.shortcuts import get_object_or_404
 
 
 def gallery(request):
+    """ This view handles showing all the categories images """
     categories = Category.objects.all()
-    selected_category = request.GET.get('category')
+    category = None
     images = Image.objects.all()
-
-    # Only filter images if a category is selected
-    if selected_category:
-        images = images.filter(category__slug=selected_category)
 
     context = {
         'categories': categories,
-        'selected_category': selected_category,
+        'category': category,
+        'images': images,
+    }
+    return render(request, 'gallery/gallery.html', context)
+
+
+def category_detail(request, slug):
+    """ This view handles specific category pages for SEO friendly urls """
+    category = get_object_or_404(Category, slug=slug)
+    categories = Category.objects.all()
+    images = Image.objects.filter(category=category)
+
+    context = {
+        'categories': categories,
+        'category': category,
         'images': images,
     }
     return render(request, 'gallery/gallery.html', context)

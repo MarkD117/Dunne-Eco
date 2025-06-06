ccb77d1e1220a0bf5f024a1b1d598f99bed32ef8
Mark Dunne <Markdunne117@gmail.com>
2025-06-06 07:38:23 +0000

    Update Gallery & FAQ admin files to reflect editable slug changes

--- gallery/admin.py
+++ gallery/admin.py
@@ -5,18 +5,8 @@ from .models import Category, Image
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
-    # Slug read-only due to auto-generation
-    readonly_fields = ('slug',)
-    # Slug included as read-only in the form layout
     fields = ('name', 'slug')
-
-    def save_model(self, request, obj, form, change):
-        """
-        Override save_model to ensure the slug is generated on save.
-        """
-        if not obj.slug:  # Only generate slug if it's not already set
-            obj.slug = obj.save()
-        super().save_model(request, obj, form, change)
+    prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Image)
